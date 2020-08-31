c8db2654e075968a1e87f48811a7b27e55a5554a
darren <darren@m4jam.com>
2020-08-31 12:34:00 +0200

    Using SQL

--- products/api.py
+++ products/api.py
@@ -5,6 +5,9 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from .serializers import ProductSerialier
 from .models import Product
+import timeit
+from django.db import connection
+import json
 
 
 class ProductViewSet(viewsets.ModelViewSet): #, ListCreateAPIView):
@@ -23,26 +26,45 @@ class ProductViewSet(viewsets.ModelViewSet): #, ListCreateAPIView):
     # paginate_by = 10
 
     def create(self, request, *args, **kwargs):
-
+        start = timeit.default_timer()
         result = []
         headers = {}
 
-        for data in request.data:
+        # for data in request.data:
+        #     pass
+            # try:
+            #
+            #     new_product = Product(**data)
+            #     new_product.clean()
+            #     new_product.save()
+            #     result.append("Added product SKU: {}.".format(data.get("sku", "Unknown")))
+            #
+            # except Exception as e:
+            #
+            #     result.append(
+            #         "Skipping product SKU: {}; because of error: {}".format(data.get("sku", "Unknown"), e)
+            #     )
 
-            try:
+        data = []
+        for prod in request.data:
+            record = (
+                prod['sku'],
+                prod['name'],
+                str(prod['attributes']),
+            )
+            data.append(record)
 
-                new_product = Product(**data)
-                new_product.clean()
-                new_product.save()
-                result.append("Added product SKU: {}.".format(data.get("sku", "Unknown")))
+        mySql_insert_query = """
+          INSERT INTO products_product (sku, name, attributes)
+          VALUES (%s, %s, %s)
+        """
 
-            except Exception as e:
-
-                result.append(
-                    "Skipping product SKU: {}; because of error: {}".format(data.get("sku", "Unknown"), e)
-                )
+        with connection.cursor() as cursor:
+            cursor.executemany(mySql_insert_query, data)
 
         serializer = self.serializer_class
         headers = self.get_success_headers(serializer.data)
-
-        return Response(result, status=status.HTTP_201_CREATED, headers=headers)
+        stop = timeit.default_timer()
+        result.append("time: {}".format(stop - start))
+        response = Response(result, status=status.HTTP_201_CREATED, headers=headers)
+        return response
